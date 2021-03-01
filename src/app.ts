6cf52d1911a48b5b88506ef8b9ea09f795fed4da
Adrien Rochedy <adrien.rochedy@gmail.com>
2021-03-01 17:22:28 +0100

    change route folomi by sport

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // routes
-app.use("/folomi",folomiRouter);
+app.use("/sport",folomiRouter);
 app.use("/auth", authRouter);
 
 app.set("x-powered-by", false);
